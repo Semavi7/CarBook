1281810fc6c3731b14f4fdd224c4e2d2f8067220
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-02-21 17:44:25 +0300

    BookCar

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarBook.Application.Features.Mediator.Queries.TestimonialQueries;
+using CarBook.Application.Features.Mediator.Results.TestimonialResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    class GetPricingQueryHandler : IRequestHandler<GetTestimonialQuery, List<GetTestimonialQueryResult>>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public GetPricingQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetTestimonialQueryResult>> Handle(GetTestimonialQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetTestimonialQueryResult
+            {
+                TestimonialID = x.TestimonialID,
+                Name = x.Name,
+                Comment = x.Comment,
+                Title = x.Title,
+                ImageUrl = x.ImageUrl
+            }).ToList();
+        }
+    }
+}
--- Frontends/CarBook.WebUI/Controllers/CarController.cs
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -1,25 +1,27 @@
-﻿using CarBook.Dto.TestimonialDtos;
+﻿using CarBook.Dto.CarDtos;
+using CarBook.Dto.ServiceDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
+namespace CarBook.WebUI.Controllers
 {
-    public class _TestimonialComponentPartial : ViewComponent
+    public class CarController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        public CarController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
-        public async Task<IViewComponentResult> InvokeAsync()
+
+        public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7062/api/Testimonials");
+            var responseMessage = await client.GetAsync("https://localhost:7062/api/Cars/GetCarWithBrand");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
                 return View(values);
             }
             return View();
