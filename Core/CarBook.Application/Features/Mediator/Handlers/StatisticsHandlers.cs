ff52dc5bb2d7740a02a7e3267d046b716878f063
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-03-11 22:17:09 +0300

    Carbook

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/StatisticsHandlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarBook.Application.Features.Mediator.Queries.StatisticsQueries;
+using CarBook.Application.Features.Mediator.Results.StatisticsResults;
+using CarBook.Application.Interfaces;
+using CarBook.Application.Interfaces.CarInterface;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers
+{
+    public class StatisticsHandlers : IRequestHandler<GetCarCountQuery, GetCarCountQueryResult>
+    {
+        private readonly ICarRepository _repository;
+
+        public StatisticsHandlers(ICarRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCarCountQueryResult> Handle(GetCarCountQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetCarCount();
+            return new GetCarCountQueryResult
+            {
+                CarCount = value,
+            };
+        }
+    }
+}
--- Core/CarBook.Application/Interfaces/CarInterface/ICarRepository.cs
+++ Core/CarBook.Application/Interfaces/CarInterface/ICarRepository.cs
@@ -11,5 +11,6 @@ namespace CarBook.Application.Interfaces.CarInterface
     {
         List<Car> GetCarsListWithBrands();
         List<Car> GetLast5CarsWithBrands();
+        int GetCarCount();
     }
 }
