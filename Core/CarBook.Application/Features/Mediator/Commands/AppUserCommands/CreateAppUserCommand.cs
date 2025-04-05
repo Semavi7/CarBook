88bb957f03ce595a8375c95b27b4e3e7586dbf0b
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-04-05 21:29:48 +0300

    CarBook

--- Core/CarBook.Application/Features/Mediator/Commands/AppUserCommands/CreateAppUserCommand.cs
+++ Core/CarBook.Application/Features/Mediator/Commands/AppUserCommands/CreateAppUserCommand.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
 
-namespace CarBook.Domain.Entities
+namespace CarBook.Application.Features.Mediator.Commands.AppUserCommands
 {
-    public class AppUser
+    public class CreateAppUserCommand : IRequest
     {
-        public int AppUserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
-        public int AppRoleId { get; set; }
-        public AppRole Approle { get; set; }
     }
 }
---
+++ Frontends/CarBook.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.RegisterDtos;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CarBook.Dto.LoginDtos;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateLoginDto createRegisterDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            
+            return View();
+        }
+    }
+}
